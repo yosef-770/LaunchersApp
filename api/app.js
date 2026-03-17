9a06124fc29685bfdfd68d0da104cb769647d6c3
Yosef <yavitan1234@gmail.com>
2026-03-17 15:09:50 +0200

    add: Referring to the auth route, and adding the first user when running the application

--- api/app.js
+++ api/app.js
@@ -4,6 +4,9 @@ import cors from 'cors'
 import { connectDB } from './db.js'
 import aunchersRouter from './routes/launchers.route.js'
 import errorHandler from './middleware/errorHandler.js'
+import authRouter from './routes/auth.route.js'
+import * as authService from './services/auth.service.js'
+
 
 
 const app = express();
@@ -12,13 +15,16 @@ const PORT = process.env.PORT;
 app.use(cors())
 app.use(express.json())
 app.use('/api/launchers', aunchersRouter)
+app.use('/api/auth', authRouter)
+
 app.use(errorHandler)
 
-connectDB().then(()=> {
+connectDB().then(async () => {
+    await authService.register({ username: 'admin', password: '770770', email: 'yossi@idf.co.il', user_type: 'admin' })
     app.listen(PORT, () => {
         console.log('Server running on port', PORT)
     })
-}) .catch((err) => {
+}).catch((err) => {
     console.error('MongoDB failed', err)
 })
 
