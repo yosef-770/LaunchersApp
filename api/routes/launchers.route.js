2f4871890142fee7077de02185b48591ebacf5fa
Yosef <yavitan1234@gmail.com>
2026-03-17 12:48:50 +0200

    add: Updating the router of destroyed launchers and adding a layer of authentication to the router

--- api/routes/launchers.route.js
+++ api/routes/launchers.route.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import * as launchersController from '../controllers/launchers.controller.js'
+import { authenticateJWT, adminPermission, writePermission, destroyedPermission } from '../middleware/auth'
 
 const aunchersRouter = express.Router()
+
+aunchersRouter.use(authenticateJWT)
 
 aunchersRouter.get('/', launchersController.getAllLauncher)
 aunchersRouter.get('/:id', launchersController.getLauncherByID)
 
-aunchersRouter.post('/', launchersController.craeteLauncher)
-aunchersRouter.put('/:id', launchersController.updateLauncher)
+aunchersRouter.post('/', writePermission, launchersController.craeteLauncher)
+aunchersRouter.put('/:id', writePermission, launchersController.updateLauncher)
 
-aunchersRouter.delete('/:id', launchersController.deleteLauncher)
+aunchersRouter.put('/:id/destroyed', writePermission, launchersController.destroyedLauncher)
+
+aunchersRouter.delete('/:id', writePermission, launchersController.deleteLauncher)
+
 
 export default aunchersRouter
